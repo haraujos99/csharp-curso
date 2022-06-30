f774fd9d5ce5d051c40b1b21350f6538aa106999
LUIZ HENRIQUE ARAUJO SALES <luiz.sales@bgp.local>
2022-06-30 20:03:38 -0300

    First commit

---
+++ Fundamentos/Comentários.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.Fundamentos {
+    class Comentários {
+        public static void Executar() {
+
+            //Cuidado com os comentários desnecessários
+            Console.WriteLine("Código claro é sempre melhor!");
+
+            /*
+             * 
+             * 
+             * 
+             */
+
+            Console.WriteLine("O C# tem xml coments");
+        }
+    }
+}
